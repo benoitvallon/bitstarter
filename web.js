df76eaa887edc735129c22a84953df474c5c459c
Ubuntu <ubuntu@ip-10-241-45-222.eu-west-1.compute.internal>
2013-07-09 07:47:22 +0000

    Test

--- web.js
+++ web.js
@@ -7,10 +7,7 @@ var app = express.createServer(express.logger());
 //  response.send('Hello World 2!');
 //});
 
-fs.readFile('/index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
+fs.readFileSync('/index.html');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
