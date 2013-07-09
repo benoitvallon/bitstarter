bc6a6ae2bf0a1f3caedaa22fb7f87796970392a8
Ubuntu <ubuntu@ip-10-241-45-222.eu-west-1.compute.internal>
2013-07-09 19:52:25 +0000

    New grader

--- grader.js
+++ grader.js
@@ -20,12 +20,14 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
-
+var sys = require('util');
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://secure-mountain-7574.herokuapp.com"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +37,17 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var getUrl = function(url) {
+   rest.get(url).on('complete', function(result) {
+      if (result instanceof Error) {
+         sys.puts('Error: ' + result.message);
+         this.retry(5000); // try again after 5 sec
+      } else {
+         sys.puts(result);
+      }
+});
+}; 
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,6 +78,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to check', clone(getUrl), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
